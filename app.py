de1c25b2d1bf836b54fe53cacf91a6b1ce8dedfa
MAYANK SHARMA <sharmahost7@gmail.com>
2026-06-29 14:00:00 +0530

    Add files via upload
    Smart healthcare chatbot built with Python, Flask, and Gemini API.

---
+++ app.py
@@ -0,0 +1,30 @@
+
+from flask import Flask, render_template, request, jsonify
+from google import genai
+
+app = Flask(__name__)
+
+# Client setup
+client = genai.Client(api_key="AIzaSyA0NYskjcJ8s0wnLzN95hvapHva0dbCHKQ")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.json['message']
+
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=f"You are a medical assistant. Give simple advice: {user_input}"
+        )
+
+        return jsonify({'reply': response.text})
+
+    except Exception as e:
+        return jsonify({'reply': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
